e15c4c6cc748122afb9866423f56be2b24eda798
oyelakinG9 <oyelakinr37@gmail.com>
2022-01-12 21:11:51 +0100

    chore: Add server file, modify webpack and package file and update readme

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Management',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
   },
 };
